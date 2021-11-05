90a239b3a5ba32bd44882447edabd861c450e3ba
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-11-05 10:35:42 -0300

    Update PlantType.java

--- src/agriculture/entities/PlantType.java
+++ src/agriculture/entities/PlantType.java
@@ -1,6 +1,7 @@
 package agriculture.entities;
 
 import agriculture.content.*;
+import arc.*;
 import arc.util.*;
 import mindustry.type.*;
 
@@ -19,7 +20,7 @@ public class PlantType {
     public String name;
 
     public PlantType(String name, Item item, int itemAmount){
-        this.name = name;
+        this.name = Core.bundle.get(name);
         this.item = item;
         this.itemAmount = itemAmount;
 
