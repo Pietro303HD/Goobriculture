0273928f71a0a8045b5613ee2e7329ad8cd9207f
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-11-05 11:41:04 -0300

    it's a pietro convention to always have more than one commit in a pr

--- src/agriculture/entities/PlantType.java
+++ src/agriculture/entities/PlantType.java
@@ -21,7 +21,7 @@ public class PlantType {
     public String displayName;
 
     public PlantType(String name, Item item, int itemAmount){
-        this.name = name
+        this.name = name;
         this.displayName = Core.bundle.get("plant." + name + ".name");
         this.item = item;
         this.itemAmount = itemAmount;
